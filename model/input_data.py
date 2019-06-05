0cd055e1218a95e1757f5f721509716891300957
root <kykareky05@mail.ru>
2019-06-05 10:48:20 +0300

    24

--- model/input_data.py
+++ model/input_data.py
@@ -1,21 +1,7 @@
-path_to_video = "C:\\videoaudioConverter test\\Source video"
+path_to_backupexe = "C:\\Program Files (x86)\\ISS\\SecurOS\\backup.exe"
+output_folder = "C:\\videoaudioConverter_test\\Output\\"
+path_to_archive = " C:\\videoaudioConverter_test\\Source_video"
+begin_time = "18-03-10 18:00:00"
 Checksums = "C:\\videoaudioConverter test\\Checksums"
 
-Set archive path= "" # директория откуда брать файлы архива
-Set output directory= "C:\\videoaudioConverter test\\Output"# путь к папке,куда будет происходить экспорт.
 
-#*ВАЖНО: Путь к архиву и выходной путь не должны заканчиваться слэшем*
-
-SecurOs Dir= "C:\Program Files (x86)\ISS\SecurOS" #папка, где лежит секурос.
-Set start Export time= время начала архива(18-03-10 18:00:00)
-Set end Export time= время конца архива(текущее время)
-
-
-проверка:
-H263_id
-MxPEG_id
-MPEG4_id
-Yuan_id
-MJPEG_id
-H264_id
-VR_id
--- test/test_backup_exe.py
+++ test/test_backup_exe.py
@@ -1,9 +1,113 @@
-import subprocess
 import time
 import datetime as dt
+from pywinauto.application import Application
+from model.input_data import *
+import hashlib
+# import subprocess
 
-def test_1():
+def test_30_H264():
     m = dt.datetime.now()
-    tm = m.strftime("%Y%m%dT%H%M%S%Z")
-    # subprocess.check_call([r"C:\Program Files (x86)\ISS\SecurOS\backup.exe", "your", "arguments", "comma", "separated"])
-    subprocess.check_call([r"C:\Program Files (x86)\ISS\SecurOS\backup.exe", "--out<C:\\videoaudioConverter test\\Output>", "--cam<33>", "--from<18-03-10 18:00:00>", "--to<"+tm+">", "--archive-folder<C:\\videoaudioConverter test\\Source video>"])
+    end_time = m.strftime("%Y%m%dT%H%M%S%Z")
+    cam_id = "30"
+    out_file_name = "30_H264.avi"
+    app = Application(backend="uia").start(r'"'+path_to_backupexe+'" --out "'+output_folder+out_file_name+'" --cam "' + cam_id + '" --from "'+begin_time+'" --to "' + end_time + '" --archive-path "'+path_to_archive+'"').connect(title='Утилита экспорта медиа данных')
+    dlg = app.window(title='Утилита экспорта медиа данных')
+    dlg1 = dlg.child_window(auto_id="2")
+    dlg1.wait('visible', timeout=150)
+    dlg.child_window(auto_id="2").click()
+
+    hasher = hashlib.md5()
+    with open(''+output_folder + out_file_name+'', 'rb') as afile:
+        buf = afile.read()
+        hasher.update(buf)
+    print(hasher.hexdigest())
+
+def test_31_H263():
+    m = dt.datetime.now()
+    end_time = m.strftime("%Y%m%dT%H%M%S%Z")
+    cam_id = "31"
+    out_file_name = "31_H263.avi"
+    app = Application(backend="uia").start(r'"' + path_to_backupexe + '" --out "' + output_folder + out_file_name + '" --cam "' + cam_id + '" --from "' + begin_time + '" --to "' + end_time + '" --archive-path "' + path_to_archive + '"').connect(title='Утилита экспорта медиа данных')
+    dlg = app.window(title='Утилита экспорта медиа данных')
+    dlg1 = dlg.child_window(auto_id="2")
+    dlg1.wait('visible', timeout=150)
+    dlg.child_window(auto_id="2").click()
+
+    hasher = hashlib.md5()
+    with open(''+output_folder + out_file_name+'', 'rb') as afile:
+        buf = afile.read()
+        hasher.update(buf)
+    print(hasher.hexdigest())
+
+def test_32_MPEG4():
+    m = dt.datetime.now()
+    end_time = m.strftime("%Y%m%dT%H%M%S%Z")
+    cam_id = "32"
+    out_file_name = "32_MPEG4.avi"
+    app = Application(backend="uia").start(r'"' + path_to_backupexe + '" --out "' + output_folder + out_file_name + '" --cam "' + cam_id + '" --from "' + begin_time + '" --to "' + end_time + '" --archive-path "' + path_to_archive + '"').connect(title='Утилита экспорта медиа данных')
+    dlg = app.window(title='Утилита экспорта медиа данных')
+    dlg1 = dlg.child_window(auto_id="2")
+    dlg1.wait('visible', timeout=150)
+    dlg.child_window(auto_id="2").click()
+
+    hasher = hashlib.md5()
+    with open(''+output_folder + out_file_name+'', 'rb') as afile:
+        buf = afile.read()
+        hasher.update(buf)
+    print(hasher.hexdigest())
+
+def test_33_MJPEG():
+    m = dt.datetime.now()
+    end_time = m.strftime("%Y%m%dT%H%M%S%Z")
+    cam_id = "33"
+    out_file_name = "33_MJPEG.avi"
+    app = Application(backend="uia").start(r'"' + path_to_backupexe + '" --out "' + output_folder + out_file_name + '" --cam "' + cam_id + '" --from "' + begin_time + '" --to "' + end_time + '" --archive-path "' + path_to_archive + '"').connect(title='Утилита экспорта медиа данных')
+    dlg = app.window(title='Утилита экспорта медиа данных')
+    dlg1 = dlg.child_window(auto_id="2")
+    dlg1.wait('visible', timeout=150)
+    dlg.child_window(auto_id="2").click()
+
+    hasher = hashlib.md5()
+    with open(''+output_folder + out_file_name+'', 'rb') as afile:
+        buf = afile.read()
+        hasher.update(buf)
+    print(hasher.hexdigest())
+
+def test_34_YUAN():
+    m = dt.datetime.now()
+    end_time = m.strftime("%Y%m%dT%H%M%S%Z")
+    cam_id = "34"
+    out_file_name = "34_YUAN.avi"
+    app = Application(backend="uia").start(r'"' + path_to_backupexe + '" --out "' + output_folder + out_file_name + '" --cam "' + cam_id + '" --from "' + begin_time + '" --to "' + end_time + '" --archive-path "' + path_to_archive + '"').connect(title='Утилита экспорта медиа данных')
+    dlg = app.window(title='Утилита экспорта медиа данных')
+    dlg1 = dlg.child_window(auto_id="2")
+    dlg1.wait('visible', timeout=150)
+    dlg.child_window(auto_id="2").click()
+
+    hasher = hashlib.md5()
+    with open(''+output_folder + out_file_name+'', 'rb') as afile:
+        buf = afile.read()
+        hasher.update(buf)
+    print(hasher.hexdigest())
+
+def test_35_MIX():
+    m = dt.datetime.now()
+    end_time = m.strftime("%Y%m%dT%H%M%S%Z")
+    cam_id = "35"
+    out_file_name = "35_MIX.avi"
+    app = Application(backend="uia").start(r'"' + path_to_backupexe + '" --out "' + output_folder + out_file_name + '" --cam "' + cam_id + '" --from "' + begin_time + '" --to "' + end_time + '" --archive-path "' + path_to_archive + '"').connect(title='Утилита экспорта медиа данных')
+    dlg = app.window(title='Утилита экспорта медиа данных')
+    dlg1 = dlg.child_window(auto_id="2")
+    dlg1.wait('visible', timeout=150)
+    dlg.child_window(auto_id="2").click()
+
+    hasher = hashlib.md5()
+    with open(''+output_folder + out_file_name+'', 'rb') as afile:
+        buf = afile.read()
+        hasher.update(buf)
+    print(hasher.hexdigest())
+
+
+    # subprocess.check_call(["C:\\Program Files (x86)\\ISS\\SecurOS\\backup.exe", "--out=C:\\videoaudioConverter_test\\Output\\out.avi", "--cam=33", "--from=18-03-10 18:00:00", "--to="+tm+"", "--archive-path=C:\\videoaudioConverter_test\\Source_video"]) вызов через другую библиотеку
+    #dlg.child_window(auto_id="2").wait('visible',  timeout=50, retry_interval=0.5)  еще один вариант ожидания, на всякий случай
+
