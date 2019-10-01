0127ae3dc716280093972273b6d1b7de6bbf3efc
root <kykareky05@mail.ru>
2019-10-01 19:26:52 +0300

    24

--- test/test_1_mpeg4_pcm_cd_vq0_aq100_fd1_asf.py
+++ test/test_1_mpeg4_pcm_cd_vq0_aq100_fd1_asf.py
@@ -24,7 +24,7 @@ def test_30_H264():
         buf = afile.read()
         hasher.update(buf)
     print(hasher.hexdigest())
-    #assert hasher.hexdigest() == hash1_H264
+    assert hasher.hexdigest() == hash1_H264
     os.remove(''+output_folder + out_file_name+'')
 
 
